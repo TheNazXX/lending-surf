f843a12cd69efaaae296516333a29b458615893d
Nazar <nz.shevchuk.2002@gmail.com>
2023-09-27 19:00:00 +0300

    header section + start surf section

--- app/js/main.js
+++ app/js/main.js
@@ -7,13 +7,20 @@ $(function(){
     nextArrow: $(".header-slick-next-btn"),
     asNavFor: ".slider-dots",
     autoplay: true,
-    autoplaySpeed: 5000,
+    autoplaySpeed: 4000,
   });
 
   $(".slider-dots").slick({
     slidesToShow: 4,
     slidesToScroll: 4,
     asNavFor: ".header__slider",
+  })
+
+  $(".surf-slider").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: $(".surf-slick-prev-btn"),
+    nextArrow: $(".surf-slick-next-btn"),
   })
   
 });
